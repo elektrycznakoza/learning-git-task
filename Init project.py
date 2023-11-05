c664a0924a84aadb50fa2a7a8cef9c4b142172f9
Leszek Stanisławski <leszek.stanislawski@2n-everpol.pl>
2023-11-05 04:02:57 +0100

    Init

--- Init project.py
+++ Init project.py
@@ -1,1 +1,29 @@
 # zadanie: pierwsze samodzielne commity
+# -*- coding: utf-8 -*-
+
+# punkt 5
+shopping_list = {
+    "grocery_store": ["oranges", "bananas", "milk"],
+    "bakery_store": ["bread", "eggs", "butter"],
+    "convenience_store": ["water", "apple juice", "chocolate"]
+}
+
+# Inicjalizacja zmiennej na sumę produktów
+total_products = 1
+
+# Iteracja po słowniku i wyświetlanie informacji o zakupach
+# punkt 6
+for store, items in shopping_list.items():
+    # Przetwarzanie nazw sklepów i produktów na wielkie litery
+# punkt 7
+    store_name_capitalized = store.capitalize()
+    items_name_capitalized = [item.capitalize() for item in items]
+
+    # Wyświetlenie informacji o zakupach w danym sklepie
+    print(f"Idę do {store_name_capitalized} i kupuję tam {', '.join(items_name_capitalized)}")
+    
+    # Zliczanie produktów
+    total_products += len(items)
+
+# Wyświetlenie sumy produktów
+print(f"W sumie kupuję {total_products} produktów.")
